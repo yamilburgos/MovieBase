69ade3935f469a7e8c72b686d9501c2ec5dfa8fa
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-11-25 12:48:04 -0500

    Expanded on MoviesController

--- MovieBase/Controllers/MoviesController.cs
+++ MovieBase/Controllers/MoviesController.cs
@@ -10,6 +10,22 @@ namespace MovieBase.Controllers {
             Movie movie = new Movie() { Name = "Shrek!" };
             // Calls Random.cshtml in Views/Movies for display.
             return View(movie);
+            /// Alternative way to return a view result.
+            /// return new ViewResult();
+        }
+
+        // Called when going to Movies/Edit/id
+        public ActionResult Edit(int id) {
+            // Displays a blank screen displaing id= and a number.
+            return Content("id=" + id);
+        }
+
+        // Called when going to Movies
+        public ActionResult Index(int pageIndex = 1, string sortBy = "Name") {
+            // Checks to see if this string was set to null or only white space.
+            sortBy = string.IsNullOrWhiteSpace(sortBy) ? "Name" : sortBy;
+            // Return a blank screen displaying the following information provided.
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
