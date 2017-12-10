08ed73d73dca05682ac1fac09284691a76d85d93
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-12-09 20:06:05 -0500

    Changed Customer's Name column to be required and altered string length

--- MovieBase/Models/Customer.cs
+++ MovieBase/Models/Customer.cs
@@ -1,9 +1,12 @@
-﻿namespace MovieBase.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieBase.Models {
 
     public class Customer {
         // Properties used when presenting states.
         public int Id { get; set; }
-        public string Name { get; set; }
+        // Data Annotations that adds properties to the database.
+        [Required] [StringLength(255)] public string Name { get; set; }
         public bool IsSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
