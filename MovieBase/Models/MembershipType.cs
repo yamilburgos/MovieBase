0293246c95130b425b8d66fb76464ecf29c072ea
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-12-10 15:42:54 -0500

    Customer page now display Membership Type for each individual customer

--- MovieBase/Models/MembershipType.cs
+++ MovieBase/Models/MembershipType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MovieBase.Models {
 
@@ -6,6 +6,7 @@ namespace MovieBase.Models {
         // Properties used when presenting membership.
         // Using these datatypes as values are very small.
         public byte Id { get; set; }
+        [Required] public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
