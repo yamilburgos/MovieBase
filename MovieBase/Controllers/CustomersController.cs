9df89bdb76288ebd8989b5d88165b22f429ae148
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-12-09 20:47:55 -0500

    Added discount rate to be displayed on the customer page

--- MovieBase/Controllers/CustomersController.cs
+++ MovieBase/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using MovieBase.Models;
@@ -22,15 +22,15 @@ namespace MovieBase.Controllers {
         public ViewResult Index() {
             // Calls Index.cshtml in Views/Customers to display a list of customers.
             // Gets all customers from the database. A DBSet that has been defined.
-            // ToList() helps executes the query for this property.
-            return View(_context.Customers.ToList());
+            // ToList() helps executes the query for this property. Include() will
+            // also past the MembershipType object/property to view as well.
+            return View(_context.Customers.Include(c => c.MembershipType).ToList());
         }
 
         // Called when going to Customers/Details/id
         public ActionResult Details(int id) {
             // A query that returns database customer(s) with matching ids (if possible).
-            Customer customer = (from thisCustomer in _context.Customers where
-               thisCustomer.Id == id select thisCustomer).SingleOrDefault();
+            Customer customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             // If this customer cannot be found, simply return this error page.
             if (customer == null) return HttpNotFound();
