1d6ca641f94a1831b161ea91b8bb1e31962fde96
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-12-09 20:32:24 -0500

    Made changes to CustomerController to now display data from database

--- MovieBase/Controllers/CustomersController.cs
+++ MovieBase/Controllers/CustomersController.cs
@@ -5,30 +5,37 @@ using MovieBase.Models;
 
 namespace MovieBase.Controllers {
     public class CustomersController : Controller {
+        // Used to access the database for this class.
+        private ApplicationDbContext _context;
+
+        public CustomersController() {
+            // Initialized upon calling this controller during run-time.
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing) {
+            // Created in order to properly dispose this object.
+            _context.Dispose();
+        }
 
         // Called when going to Customers
         public ViewResult Index() {
             // Calls Index.cshtml in Views/Customers to display a list of customers.
-            return View(GetCustomers());
+            // Gets all customers from the database. A DBSet that has been defined.
+            // ToList() helps executes the query for this property.
+            return View(_context.Customers.ToList());
         }
 
         // Called when going to Customers/Details/id
         public ActionResult Details(int id) {
-            // Uses a query that returns a customer with a matching id (if possible).
-            Customer customer = (from thisCustomer in GetCustomers() where
+            // A query that returns database customer(s) with matching ids (if possible).
+            Customer customer = (from thisCustomer in _context.Customers where
                thisCustomer.Id == id select thisCustomer).SingleOrDefault();
+
             // If this customer cannot be found, simply return this error page.
             if (customer == null) return HttpNotFound();
             // Calls Details.cshtml in Views/Customers to display a single customer.
             return View(customer);
-        }
-
-        private List<Customer> GetCustomers() {
-            return new List<Customer> {
-                // Creates new customer instances to be loaded up.
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
