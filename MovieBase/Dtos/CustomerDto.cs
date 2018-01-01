49ad0aae41909cba007f95921bde7c971a20829e
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-01-01 15:40:48 -0500

    Created New Dto Folder to Hold Domain Classes for the API Specifically

---
+++ MovieBase/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieBase.Models {
+
+    public class CustomerDto {
+        // Properties used when presenting states.
+        public int Id { get; set; }
+
+        // Data Annotations that adds properties to the database.
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribed { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
