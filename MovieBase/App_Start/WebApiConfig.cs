faace5ccf4fc914e8e6ad271b6aa06ccda9286f8
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-01-01 16:20:30 -0500

    Changed Web API Notation to Camel Notation

--- MovieBase/App_Start/WebApiConfig.cs
+++ MovieBase/App_Start/WebApiConfig.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
-namespace MovieBase
-{
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
+namespace MovieBase {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
