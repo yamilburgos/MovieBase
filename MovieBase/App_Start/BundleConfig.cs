6bfec8c1a6717c83380ede07bfad14f0a7c53424
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-01-07 15:45:25 -0500

    Added BootBox to Project For Simpler Access to Bootstrap's CSS Styling

--- MovieBase/App_Start/BundleConfig.cs
+++ MovieBase/App_Start/BundleConfig.cs
@@ -17,6 +17,7 @@ namespace MovieBase {
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
