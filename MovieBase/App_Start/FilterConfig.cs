ce3e053162baa99b6ef75e847179c1949c46ee0a
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-01-14 17:59:19 -0500

    Added Additional Controllers For Account & Manage. Fixed Several Bugs

--- MovieBase/App_Start/FilterConfig.cs
+++ MovieBase/App_Start/FilterConfig.cs
@@ -1,12 +1,8 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace MovieBase
-{
-    public class FilterConfig
-    {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
+namespace MovieBase {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
         }
     }
---
+++ MovieBase/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieBase.Startup))]
+namespace MovieBase {
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
