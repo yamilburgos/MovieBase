2c554e56c4a4482c1173c1091dbcb81f8eba5111
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-01-20 14:56:29 -0500

    Created a New Rentals Form & Added it to the Navigation Bar

---
+++ MovieBase/Controllers/RentalsController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace MovieBase.Controllers {
+    public class RentalsController : Controller {
+        // Called when going to Movies/New
+        public ActionResult New() {
+            return View();
+        }
+    }
+}
